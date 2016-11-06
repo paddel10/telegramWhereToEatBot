a1a262462da0ddc94f5b292aac467214850426e0
paddel10 <root@gmail.com>
2016-11-06 16:50:04 +0100

    add config

---
+++ src/config.php
@@ -0,0 +1,7 @@
+<?php
+define('BOT_TOKEN', 'XXXXXXXXX:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX');
+define('BOT_WEBHOOK', 'https://bot.server/poll-bot-webhook.php');
+define('TGRAM_DB', 'my_db');
+define('TGRAM_TABLE', 'my_table');
+define('TGRAM_USER', 'my_user');
+define('TGRAM_PWD', 'my_pwd');
