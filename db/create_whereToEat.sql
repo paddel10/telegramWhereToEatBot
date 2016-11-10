1ba0c1c90695144cb48b449325ded3634ec8d118
paddel10 <paddel10@hotmail.com>
2016-11-10 22:36:56 +0100

    sql corrections

--- db/create_whereToEat.sql
+++ db/create_whereToEat.sql
@@ -5,6 +5,6 @@ SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
 DROP TABLE IF EXISTS `whereToEat`;
 CREATE TABLE `whereToEat` (
-  `key` text NOT NULL,
-  `value` text NOT NULL
+  `k` text NOT NULL,
+  `v` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
