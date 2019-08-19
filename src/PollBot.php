8398e306d0437886d13c43d82d66eb3144c34d93
patrick <paddel10@hotmail.com>
2019-08-19 18:53:43 +0200

    add GetMenuLinde

--- src/PollBot.php
+++ src/PollBot.php
@@ -498,7 +498,10 @@ class PollBotChat extends TelegramBotChat {
   protected function sendMenu($location) {
       switch ($location) {
           case 'weinberg':
-	      $this->apiSendPhoto('https://patland.ch/tgram/whereToEat/src/menu/weinberg.png');
+              $this->apiSendPhoto('https://patland.ch/tgram/whereToEat/src/menu/weinberg.png');
+              break;
+          case 'linde':
+              $this->apiSendPhoto('https://patland.ch/tgram/whereToEat/src/menu/linde.png');
               break;
           default:
               break;
