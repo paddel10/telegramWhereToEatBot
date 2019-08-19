ded298d06187ef006a4df699b1a944625ca5ca7b
Patrick Reichardt <patrickr@i-web.ch>
2019-08-19 13:45:36 +0200

    define MENU_PATH

--- src/GetMenuWeinberg.php
+++ src/GetMenuWeinberg.php
@@ -1,10 +1,10 @@
 <?php
 require_once "TakeScreenshot.php";
 require_once "ExtractLink.php";
+require_once "config.php";
 
 const WEINBERG_JPG = 'weinberg.jpg';
 const WEINBERG_PNG = 'weinberg.png';
-const MENU_DIR = 'menu/';
 
 if (php_sapi_name() === 'cli') {
     $apiKey = $argv[1];
@@ -20,11 +20,11 @@ if (php_sapi_name() === 'cli') {
     }
     if (!empty($base64)) {
         $content = file_get_contents($takeScreenshot->getPath());
-        file_put_contents(MENU_DIR . WEINBERG_JPG, $content);
+        file_put_contents(MENU_PATH . WEINBERG_JPG, $content);
 
         // crop
-        $imagick = new \Imagick(MENU_DIR. WEINBERG_JPG);
+        $imagick = new \Imagick(MENU_PATH . WEINBERG_JPG);
         $imagick->cropImage(516, 650, 188, 95);
-        file_put_contents(MENU_DIR . WEINBERG_PNG, $imagick->getImageBlob());
+        file_put_contents(MENU_PATH . WEINBERG_PNG, $imagick->getImageBlob());
     }
 }
