8198e0026d167f88ccf30eed4d8f38ec9ac365ec
Patrick Reichardt <patrickr@i-web.ch>
2019-08-19 13:30:53 +0200

    add GetMenuWeinberg

--- README.md
+++ README.md
@@ -3,6 +3,20 @@ Telegram Bot automatically sets up a poll everyday so users in a group can vote 
 
 ## Usage
 ```
+# src/config.php
+<?php
+define('BOT_TOKEN', 'XXXXXXXXX:XXXX-XXXXXXXXXXXXXXXX_XXXXXXXXXXXXX');
+define('BOT_WEBHOOK', 'https://XXXX/src/poll-bot-webhook.php');
+define('CHAT_ID', XXXX);
+define('FROM_ID', XXXX);
+define('FROM_FIRST_NAME', 'patrick');
+define('CHAT_TITLE', 'WhereToEat Group');
+```
+```
+sudo php poll-bot-webhook.php set
+sudo php poll-bot-webhook.php remove
+```
+```
 sudo php poll-bot-webhook.php /endpoll
 ```
 ```
---
+++ src/GetMenuWeinberg.php
@@ -0,0 +1,30 @@
+<?php
+require_once "TakeScreenshot.php";
+require_once "ExtractLink.php";
+
+const WEINBERG_JPG = 'weinberg.jpg';
+const WEINBERG_PNG = 'weinberg.png';
+const MENU_DIR = 'menu/';
+
+if (php_sapi_name() === 'cli') {
+    $apiKey = $argv[1];
+
+    $extractLink = new ExtractLink();
+    $link = $extractLink->extract();
+    $base64 = '';
+
+    if (!empty($link)) {
+        $takeScreenshot = new TakeScreenshot($apiKey);
+        $takeScreenshot->getScreenshot($link);
+        $base64 = $takeScreenshot->getBase64();
+    }
+    if (!empty($base64)) {
+        $content = file_get_contents($takeScreenshot->getPath());
+        file_put_contents(MENU_DIR . WEINBERG_JPG, $content);
+
+        // crop
+        $imagick = new \Imagick(MENU_DIR. WEINBERG_JPG);
+        $imagick->cropImage(516, 650, 188, 95);
+        file_put_contents(MENU_DIR . WEINBERG_PNG, $imagick->getImageBlob());
+    }
+}
