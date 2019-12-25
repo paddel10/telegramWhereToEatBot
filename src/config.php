00b9b49502e563e4cd43d5bb1adcc40545f44636
paddel10 <root@gmail.com>
2019-12-25 14:07:51 +0100

    update gitignore

--- src/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('BOT_TOKEN', 'XXXXXXXXX:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX');
-define('BOT_WEBHOOK', 'https://bot.server/poll-bot-webhook.php');
-define('CHAT_ID', 'XXXX');
-define('FROM_ID', 'XXXX');
-define('FROM_FIRST_NAME', 'XXXX');
-define('CHAT_TITLE', 'XXXX');
